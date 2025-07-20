de806207fa4103ec48e2d7953517ed58226c639b
Hyung-Jin Yoon <stargaze221@gmail.com>
2025-07-20 16:55:37 -0500

    minor clean up

--- scripts/run_docker.sh
+++
@@ -1,8 +0,0 @@
-docker run --runtime nvidia --privileged -it --rm \
-    --network host \
-    --device /dev/gpiomem \
-    --device /dev/mem \
-    -v /sys:/sys \
-    -v /tmp:/tmp \
-    --ipc=host \
-    isaac_ros_dev-aarch64
